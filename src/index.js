025d84c4d584d9d741a30381d612e1cc5839dd94
Vaycheslav <slavonkozak@mail.ru>
2023-01-08 18:07:42 +0300

    add atterns validate

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import List from './containers/List'
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import List from "./containers/List";
+import {
+  InputWithNameValidation,
+  InputWithNameAndEmailValidation,
+} from "./patterns/withRequired";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <List />
+    <br />
+    <InputWithNameValidation />
+    <br />
+    <InputWithNameAndEmailValidation />
   </React.StrictMode>
 );
